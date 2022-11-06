47496727cd4d1f4f4925a461e93af2233e762647
Amelie Pira <amelie.pira@gmail.com>
2022-11-06 17:49:29 +0000

    added base client side utils

---
+++ client/src/utils/reducers.js
@@ -0,0 +1,61 @@
+import {
+  ADD_TO_CART,
+} from "./actions";
+
+const initialState = {
+  products: [],
+  categories: [],
+  currentCategory: "",
+  cart: [],
+  cartOpen: false,
+};
+
+//reducer function - arguments: initial state/state and actions
+const reducers = (state = initialState, action) => {
+  //add all reducer cases with switch/case syntax
+  switch (action.type) {
+    case ADD_TO_CART:
+      return {
+        ...state,
+        cartOpen: true,
+        cart: [...state.cart, action.product],
+      };
+    
+    case UPDATE_CATEGORIES:
+      return {
+        ...state,
+        categories: [...action.categories],
+      };
+    case UPDATE_CURRENT_CATEGORY:
+      return {
+        ...state,
+        currentCategory: action.currentCategory,
+      };
+    
+    case REMOVE_FROM_CART:
+      let newState = state.cart.filter((product) => {
+        return product._id !== action._id;
+      });
+
+      return {
+        ...state,
+        cartOpen: newState.length > 0,
+        cart: newState,
+      };
+    case CLEAR_CART:
+      return {
+        ...state,
+        cartOpen: false,
+        cart: [],
+      };
+    case TOGGLE_CART:
+      return {
+        ...state,
+        cartOpen: !state.cartOpen,
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducers;
---
+++ client/src/utils/store.js
@@ -0,0 +1,5 @@
+// import { configureStore } from "@reduxjs/toolkit";
+import { legacy_createStore as createStore } from "redux";
+import reducers from "./reducers";
+
+export default createStore(reducers);
