a30e18721a465fbc3009e5f1de1d94b51ba8eba4
Amelie Pira <amelie.pira@gmail.com>
2022-11-06 17:44:59 +0000

    adjusted cleint app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { setContext } from "@apollo/client/link/context";
+import { Provider } from "react-redux";
+
+import Home from "./containers/Home";
+import NoMatch from "./containers/NoMatch";
+import Login from "./containers/Login";
+import Signup from "./containers/Signup";
+import History from "./containers/OrderHistory";
+import Nav from "./components/Nav";
+import store from "./utils/store";
+
+const httpLink = createHttpLink({
+  uri: process.env.REACT_APP_GRAPHQL || "/graphql",
+  credentials: "same-origin",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <Router>
+        <div className="App">
+          <Provider store={store}>
+            <Nav />
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route exact path="/login" element={<Login />} />
+              <Route exact path="/signup" element={<Signup />} />
+              <Route exact path="/history" element={<History />} />
+              <Route component={<NoMatch />} />
+            </Routes>
+          </Provider>
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
 
