ca9effb8d7b8ca250e828700a399ebc8fdd8137e
Amelie Pira <amelie.pira@gmail.com>
2022-11-07 17:47:43 +0000

    added api import to home page

--- client/src/containers/Home.js
+++ client/src/containers/Home.js
@@ -3,6 +3,8 @@ import CategoryMenu from "../components/CategoryMenu";
 import Cart from "../components/Cart";
 
 //this page links to the cocktail api with this url: www.thecocktaildb.com/api/json/v1/1/search.php?f=a
+import { searchCocktailsByLetter } from "../utils/api";
+
 //we need to replace the category menu with a search form
 //we want to filter by letter - either with a drop down or with a on/off button (only 1 letter selected at a time)
 //similar to the book search, we can browse when not logged in, but the add button only appears when logged in
