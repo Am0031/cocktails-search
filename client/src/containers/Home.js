c3393bf3697938848da954d939dfab6d7aaf4782
Amelie Pira <amelie.pira@gmail.com>
2022-11-07 17:10:48 +0000

    added notes to home page

--- client/src/containers/Home.js
+++ client/src/containers/Home.js
@@ -2,12 +2,16 @@ import ProductList from "../components/ProductList";
 import CategoryMenu from "../components/CategoryMenu";
 import Cart from "../components/Cart";
 
+//this page links to the cocktail api with this url: www.thecocktaildb.com/api/json/v1/1/search.php?f=a
+//we need to replace the category menu with a search form
+//we want to filter by letter - either with a drop down or with a on/off button (only 1 letter selected at a time)
+//similar to the book search, we can browse when not logged in, but the add button only appears when logged in
+
 const Home = () => {
   return (
     <div className="container">
       <CategoryMenu />
       <ProductList />
-      <Cart />
     </div>
   );
 };
