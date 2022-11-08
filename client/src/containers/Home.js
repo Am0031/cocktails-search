17756bb8adaf75d0fa5bc2d9c8eafef925479d18
Amelie Pira <amelie.pira@gmail.com>
2022-11-08 17:25:01 +0000

    added option note

--- client/src/containers/Home.js
+++ client/src/containers/Home.js
@@ -5,9 +5,13 @@ import Cart from "../components/Cart";
 //this page links to the cocktail api with this url: www.thecocktaildb.com/api/json/v1/1/search.php?f=a
 import { searchCocktailsByLetter } from "../utils/api";
 
-//we need to replace the category menu with a search form
-//we want to filter by letter - either with a drop down or with a on/off button (only 1 letter selected at a time)
-//similar to the book search, we can browse when not logged in, but the add button only appears when logged in
+//we need to replace the category menu with options for search
+//Option 1 - we want to filter by letter - with a on/off buttons (only 1 letter selected at a time)
+//Option 2 - we can search by category - maybe a list of popular ingredients as categories to pick from
+//Option 3 - we can search by cocktail name submitted in an input field
+//create tabs so we can flip from one search type to the other
+
+//on cocktail cards, similar to the book search, we can browse when not logged in, but the add button only appears when logged in
 
 const Home = () => {
   return (
