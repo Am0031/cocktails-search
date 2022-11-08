cc8e15d857072e7559cbd30b00c4aad6a551f66d
Amelie Pira <amelie.pira@gmail.com>
2022-11-08 17:27:31 +0000

    added url for search by category

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -11,6 +11,12 @@ export const searchCocktailsByName = (query) => {
   return fetch(`www.thecocktaildb.com/api/json/v1/1/search.php?s=${query}`);
 };
 
+//search by category
+// www.thecocktaildb.com/api/json/v1/1/filter.php?c=Ordinary_Drink
+export const searchCocktailsByCategory = (query) => {
+  return fetch(`www.thecocktaildb.com/api/json/v1/1/filter.php?c=${query}`);
+};
+
 //get full cocktail details by id
 // www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11007
 export const getCocktailDetails = (id) => {
