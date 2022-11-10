2fca084c74657da5aa6945769c6e4e704389be34
Amelie Pira <amelie.pira@gmail.com>
2022-11-10 18:09:17 +0000

    added 3-way search types (wip)

--- client/src/components/CategoryMenu.js
+++ client/src/components/CategoryMenu.js
@@ -2,58 +2,113 @@ import React, { useEffect } from "react";
 import { useQuery } from "@apollo/client";
 import { useDispatch, useSelector } from "react-redux";
 
-import { UPDATE_CATEGORIES, UPDATE_CURRENT_CATEGORY } from "../utils/actions";
+import {
+  UPDATE_CATEGORIES,
+  UPDATE_CURRENT_CATEGORY,
+  UPDATE_CURRENT_LETTER,
+} from "../utils/actions";
 import { QUERY_CATEGORIES } from "../utils/queries";
 import { idbPromise } from "../utils/helpers";
+import { searchCocktailsByName } from "../utils/api";
 
 const CategoryMenu = () => {
   const dispatch = useDispatch();
   const state = useSelector((state) => state);
 
-  const { categories } = state;
+  const { searchTypes, currentSearchType, letters, categories, cocktailName } =
+    state;
 
-  const { loading, data: categoryData } = useQuery(QUERY_CATEGORIES);
+  const handleTypeClick = (type) => {
+    dispatch({
+      type: UPDATE_SEARCH_TYPE,
+      currentSearchType: type,
+    });
+  };
 
-  useEffect(() => {
-    if (categoryData) {
+  const handleLetterClick = (item) => {
+    if (currentSearchType === 0) {
       dispatch({
-        type: UPDATE_CATEGORIES,
-        categories: categoryData.categories,
-      });
-      categoryData.categories.forEach((category) => {
-        idbPromise("categories", "put", category);
-      });
-    } else if (!loading) {
-      idbPromise("categories", "get").then((categories) => {
-        dispatch({
-          type: UPDATE_CATEGORIES,
-          categories: categories,
-        });
+        type: UPDATE_CURRENT_LETTER,
+        currentLetter: item,
       });
     }
-  }, [categoryData, loading, dispatch]);
+  };
 
-  const handleClick = (id) => {
-    dispatch({
-      type: UPDATE_CURRENT_CATEGORY,
-      currentCategory: id,
-    });
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    if (currentSearchType === 2) {
+      const response = await searchCocktailsByName(cocktailName);
+
+      if (!response.ok) {
+        throw new Error("something went wrong!");
+      }
+
+      const { drinks } = await response.json();
+      dispatch({
+        type: UPDATE_CURRENT_NAME,
+        cocktails: drinks,
+      });
+    }
+  };
+
+  //function to handle input field change
+  const handleChange = (event) => {
+    dispatch({ type: UPDATE_CURRENT_NAME, cocktailName: event.target.value });
   };
 
   return (
     <div>
-      <h2>Choose a Category:</h2>
-      {categories &&
-        categories.map((item) => (
+      <h2>Choose your search type:</h2>
+      {searchTypes &&
+        searchTypes.map((i) => (
           <button
-            key={item._id}
+            key={i}
+            id={i}
             onClick={() => {
-              handleClick(item._id);
+              handleTypeClick(i);
+            }}
+          >
+            {categories[i]}
+          </button>
+        ))}
+      {currentSearchType === 0 &&
+        letters.map((item) => (
+          <button
+            key={item}
+            id={item.toLowerCase()}
+            onClick={() => {
+              handleLetterClick(item.toLowerCase());
             }}
           >
             {item.name}
           </button>
         ))}
+      {currentSearchType === 1 &&
+        categories.map((item) => (
+          <button
+            key={item}
+            id={item}
+            onClick={() => {
+              handleCategoryClick(item);
+            }}
+          >
+            {item.name}
+          </button>
+        ))}
+      {currentSearchType === 2 && (
+        <>
+          <form onSubmit={onSubmit}>
+            <input
+              style={{ width: "400px" }}
+              id="search-input"
+              value={cocktailName}
+              placeholder="Enter a cocktail name"
+              onChange={handleChange}
+            ></input>
+            <button id="submit-btn">Search</button>
+          </form>
+        </>
+      )}
     </div>
   );
 };
