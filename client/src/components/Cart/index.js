79dfd7e213b4d2ef556185239fba6355f060e313
Amelie Pira <amelie.pira@gmail.com>
2022-11-11 09:26:28 +0000

    added spinner

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { loadStripe } from "@stripe/stripe-js";
 import { useLazyQuery } from "@apollo/client";
-import { QUERY_CHECKOUT } from "../../utils/queries";
+
 import { idbPromise } from "../../utils/helpers";
 import CartItem from "../CartItem";
 import Auth from "../../utils/auth";
