d51a00cba10e960b3c5d017f75eed93a5369e986
Amelie Pira <amelie.pira@gmail.com>
2022-11-11 10:09:11 +0000

    amended Detail component

--- client/src/containers/Detail.js
+++ client/src/containers/Detail.js
@@ -1,17 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useParams } from "react-router-dom";
-import { useQuery } from "@apollo/client";
 
-import Cart from "../components/Cart";
-
-import {
-  REMOVE_FROM_CART,
-  UPDATE_CART_QUANTITY,
-  ADD_TO_CART,
-  UPDATE_PRODUCTS,
-} from "../utils/actions";
-import { QUERY_PRODUCTS } from "../utils/queries";
+import { ADD_TO_SAVED } from "../utils/actions";
 import { idbPromise } from "../utils/helpers";
 import spinner from "../assets/spinner.gif";
 
@@ -21,93 +12,56 @@ const Detail = () => {
 
   const { id } = useParams();
 
-  const [currentProduct, setCurrentProduct] = useState({});
+  const [currentCocktail, setCurrentCocktail] = useState({});
 
-  const { loading, data } = useQuery(QUERY_PRODUCTS);
-
-  const { products, cart } = state;
+  const { cocktails } = state;
 
   useEffect(() => {
     // already in global store
-    if (products.length) {
-      setCurrentProduct(products.find((product) => product._id === id));
-    }
-    // retrieved from server
-    else if (data) {
-      dispatch({
-        type: UPDATE_PRODUCTS,
-        products: data.products,
-      });
-
-      data.products.forEach((product) => {
-        idbPromise("products", "put", product);
-      });
+    if (cocktails.length) {
+      setCurrentCocktail(cocktails.find((product) => product._id === id));
     }
     // get cache from idb
     else if (!loading) {
-      idbPromise("products", "get").then((indexedProducts) => {
+      idbPromise("cocktails", "get").then((indexedcocktails) => {
         dispatch({
-          type: UPDATE_PRODUCTS,
-          products: indexedProducts,
+          type: UPDATE_COCKTAILS,
+          cocktails: indexedcocktails,
         });
       });
     }
-  }, [products, data, loading, dispatch, id]);
+  }, [cocktails, data, loading, dispatch, id]);
 
-  const addToCart = () => {
+  const addToSaved = () => {
     const itemInCart = cart.find((cartItem) => cartItem._id === id);
     if (itemInCart) {
-      dispatch({
-        type: UPDATE_CART_QUANTITY,
-        _id: id,
-        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1,
-      });
-      idbPromise("cart", "put", {
-        ...itemInCart,
-        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1,
-      });
+      console.log("already saved");
     } else {
       dispatch({
-        type: ADD_TO_CART,
-        product: { ...currentProduct, purchaseQuantity: 1 },
+        type: ADD_TO_SAVED,
+        cocktails: { ...state.cocktails, currentCocktail },
       });
-      idbPromise("cart", "put", { ...currentProduct, purchaseQuantity: 1 });
     }
-  };
-
-  const removeFromCart = () => {
-    dispatch({
-      type: REMOVE_FROM_CART,
-      _id: currentProduct._id,
-    });
-
-    idbPromise("cart", "delete", { ...currentProduct });
   };
 
   return (
     <>
-      {currentProduct && cart ? (
+      {currentCocktail ? (
         <div className="container my-1">
-          <Link to="/">← Back to Products</Link>
+          <Link to="/">← Back to Browse</Link>
 
-          <h2>{currentProduct.name}</h2>
+          <h2>{currentCocktail.name}</h2>
 
-          <p>{currentProduct.description}</p>
+          <p>{currentCocktail.description}</p>
 
           <p>
             <strong>Price:</strong>${currentProduct.price}{" "}
-            <button onClick={addToCart}>Add to Cart</button>
-            <button
-              disabled={!cart.find((p) => p._id === currentProduct._id)}
-              onClick={removeFromCart}
-            >
-              Remove from Cart
-            </button>
+            <button onClick={addToSaved}>Add to Saved</button>
           </p>
 
           <img
-            src={`/images/${currentProduct.image}`}
-            alt={currentProduct.name}
+            src={`/images/${currentCocktail.image}`}
+            alt={currentCocktail.name}
           />
         </div>
       ) : null}
