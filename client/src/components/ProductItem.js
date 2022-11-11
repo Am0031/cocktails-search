02836dee9afc7a5ca6707472f5981982cca28d9a
Amelie Pira <amelie.pira@gmail.com>
2022-11-11 16:51:48 +0000

    cleaned productItem

--- client/src/components/ProductItem.js
+++ client/src/components/ProductItem.js
@@ -10,29 +10,14 @@ const ProductItem = (item) => {
 
   const { image, name, _id } = item;
 
-  const { cocktails } = state;
-
-  const viewDetails = (id) => {
-    console.log(`going to cocktail details - ${id}`);
-    // const savedItem = cocktails.find((cartItem) => cartItem._id === _id);
-    // if (savedItem) {
-    //   console.log("already saved");
-    // } else {
-    //   dispatch({
-    //     type: ADD_TO_SAVED,
-    //     cocktails: { ...cocktails, selectedCocktail },
-    //   });
-    // }
-  };
-
   return (
     <div className="card px-1 py-1">
       <Link to={`/cocktail/${_id}`}>
         <img alt={name} src={`${image}`} />
         <p>{name}</p>
+        <button>View details</button>
       </Link>
       <div></div>
-      <button onClick={() => viewDetails(_id)}>View details</button>
     </div>
   );
 };
